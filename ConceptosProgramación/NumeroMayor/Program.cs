d758cf49fdb29ffdab1947a41f2dbe649257b534
Santiago <reyestorressanti@gmail.com>
2025-04-04 11:30:19 -0500

    Update Conceptos

---
+++ ConceptosProgramación/NumeroMayor/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Digite primer numero: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite segundo numero: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite tercer numero: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > num2 || num1 > num3)
+{
+    Console.WriteLine($"El numero mayor es: {num1}");
+}
+else if (num2 > num1 || num2 > num3)
+{
+    Console.WriteLine($"El numero mayor es: {num2}");
+}
+else if (num3 > num1 || num3 > num2)
+{
+    Console.WriteLine($"El numero mayor es: {num3}");
+}
+else
+{
+    Console.WriteLine("Los tres numeros son iguales");
+}
--- ConceptosProgramación/ParImpar/Program.cs
+++ ConceptosProgramación/ParImpar/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿do
+{
+    Console.WriteLine("Digite um número entero:");
+    int numero = int.Parse(Console.ReadLine());
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"el numero {numero} es par.");
+    }
+    else
+    {
+        Console.WriteLine($"el numero {numero} es  impar.");
+    }
+}
+while (true);
